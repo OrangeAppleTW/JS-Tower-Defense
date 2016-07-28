6917edb4e757c931c6915da9fc110d11aa78b2e1
Kevin <kevin78515@gmail.com>
2016-07-28 15:49:29 +0800

    修改 enemy 初始位置

--- v1/main.js
+++ v1/main.js
@@ -1,15 +1,15 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+var enemy = {
+  x: 96,
+  y: 480-32
+};
+
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
-
-var enemy = {
-  x: 0,
-  y: 0
-};
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
